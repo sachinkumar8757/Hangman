a19bb24cf6a094bdd7f133a003738d464b0fcf5e
Sachin Kumar <109869838+sachinkumar8757@users.noreply.github.com>
2022-07-23 23:51:41 +0530

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,96 @@
+import random
+def hangman():
+    list_of_words=['country','state','district','india','bihar','champaran']
+    word=random.choice(list_of_words)
+    turns=10
+    guessmade=''
+    valid_entry=set('abcdefghijklmnopqrstuvwxyz')
+
+    while len(word)>0:
+        main_word=""
+        missed=0
+        for letter in word:
+            if letter in guessmade:
+                main_word=main_word+letter
+            else:
+                main_word=main_word+"_ "
+        if main_word==word:
+             print(main_word)
+             print("You won!!!!!")
+             break
+
+        print("guess a world",main_word)
+        guess=input()
+
+        if guess in valid_entry:
+            guessmade=guessmade+guess
+        else:
+            guess=input("enter valid character")
+
+        if guess not in word:
+            turns=turns-1
+
+            if turns==9:
+                print("9 turn left!!")
+                print("-----------------")
+            if turns==8:
+                print("8 turn left!!")
+                print("-----------------")
+                print("     O     ")
+            if turns==7:
+                print("7 turn left!!")
+                print("-----------------")
+                print("     O     ")
+                print("     |     ")
+            if turns==6:
+                print("6 turn left!!")
+                print("-----------------")
+                print("     O     ")
+                print("     |     ")
+                print("    /     ")
+            if turns==5:
+                print("5 turn left!!")
+                print("-----------------")
+                print("     O     ")
+                print("     |     ")
+                print("    / \    ")
+            if turns==4:
+                print("4 turn left!!")
+                print("-----------------")
+                print("    \O     ")
+                print("     |     ")
+                print("    / \    ")
+            if turns==3:
+                print("3 turn left!!")
+                print("-----------------")
+                print("    \O/    ")
+                print("     |     ")
+                print("    / \    ")
+            if turns==2:
+                print("2 turn left!!")
+                print("-----------------")
+                print("    \O/ |   ")
+                print("     |     ")
+                print("    / \    ")
+            if turns==1:
+                print("only 1 turn left!! hangman on his last breath")
+                print("-----------------")
+                print("    \O/_|   ")
+                print("     |     ")
+                print("    / \    ")
+            if turns==0:
+                print("you loose")
+                print("you let a good man die")
+                print("-----------------")
+                print("      O_|    ")
+                print("     /|\     ")
+                print("     / \     ")
+                break
+       
+                
+           
+name=input("Enter your name--> ")
+print("Welcome",name,"!!" )
+print("---------------------------")
+print("Try to guess the word in less than 10 attempts")
+hangman()
